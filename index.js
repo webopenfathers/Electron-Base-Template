76442f1e3a875e1220cde4c8bb59c34942da5073
zwx1132021 <1290443925@qq.com>
2024-11-17 19:29:03 +0800

    2024-11-17 19:28 主进程与渲染进程的通信

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 
 // 创建窗口
 const mainWindow = function () {
@@ -7,10 +7,25 @@ const mainWindow = function () {
     height: 600,
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation: false,
     },
   })
 
   win.loadFile('./page/index.html')
+
+  // 渲染进程和主进程通信
+  ipcMain.on('open-dev-tools', () => {
+    win.webContents.openDevTools()
+  })
+
+  ipcMain.on('close-dev-tools', () => {
+    win.webContents.closeDevTools()
+  })
+
+  // 主进程向渲染进程发送消息
+  ipcMain.handle('get-version-info', () => {
+    return process.versions
+  })
 }
 
 // electron运行完毕---加载窗口
